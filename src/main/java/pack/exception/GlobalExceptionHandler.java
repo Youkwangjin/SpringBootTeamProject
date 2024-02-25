7c4b218fd58f7727396437e6bdf0b51759ff87e7
Kwangjin <kwangjin5468@naver.com>
2024-02-25 14:08:25 +0900

    Feat : 404, 500 에러 페이지 추가

--- src/main/java/pack/exception/GlobalExceptionHandler.java
+++ src/main/java/pack/exception/GlobalExceptionHandler.java
@@ -1,17 +1,29 @@
 package pack.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public String handleValidationExceptions() {
-        // 직접 에러 메시지를 전달하지 않고, 사용자 정의 에러 페이지로 리다이렉션
         return "redirect:/owner/error";
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public String handle404() {
+        return "error/404";
+    }
+
+    @ExceptionHandler(Exception.class)
+    public String handle500(Model model) {
+        model.addAttribute("errorMessage", "서버 내부 오류가 발생했습니다.");
+        return "error/500";
     }
 }
