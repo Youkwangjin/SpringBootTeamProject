9128614a1500ae37f699fe93dd13144aeb5c1583
Kwangjin <kwangjin5468@naver.com>
2024-02-25 14:56:47 +0900

    Chore: Update user URL to follow RESTful conventions

--- src/main/java/pack/controller/user/UserController.java
+++ src/main/java/pack/controller/user/UserController.java
@@ -20,75 +20,74 @@ import java.util.Map;
 @AllArgsConstructor
 public class UserController {
 
-
     private final UserService userService;
 
-    @GetMapping("/firstLogin")
-    public String LoginGo(HttpSession session) {
-        if (session.getAttribute("userSession") != null) {
-            return "redirect:/userSessionKeep";
-        }
-        return "user/user-login";
-    }
-
-    @GetMapping("/userJoinGo")
+    @GetMapping("/user/join")
     public String userJoinGo(HttpSession session) {
         if (session.getAttribute("userSession") != null) {
-            return "redirect:/userSessionKeep";
+            return "redirect:/user/mypage";
         }
         return "user/user-join";
     }
 
-    @GetMapping("/userLoginGo")
+    @GetMapping("/user/login")
     public String userLoginGo(HttpSession session) {
         if (session.getAttribute("userSession") != null) {
-            return "redirect:/userSessionKeep";
+            return "redirect:/user/mypage";
         }
         return "user/user-login";
     }
 
-    @GetMapping("/userUpdate")
+    @GetMapping("/user/update")
     public String userUpdatePage(Model model, HttpSession session) {
         UserDTO user = (UserDTO) session.getAttribute("userSession");
         model.addAttribute("userSession", user);
-        return "user/user-update";
+        if (user != null) {
+            return "user/user-update";
+        } else {
+            return "user/user-login";
+        }
     }
 
-    @GetMapping("/userDelete")
+    @GetMapping("/user/delete")
     public String userDeletePage(Model model, HttpSession session) {
         UserDTO user = (UserDTO) session.getAttribute("userSession");
         model.addAttribute("userSession", user);
-        return "user/user-delete";
+        if (user != null) {
+            return "user/user-delete";
+        } else {
+            return "user/user-login";
+        }
     }
 
-    @GetMapping("/userSessionKeep")
-    public String userSessionKeep(HttpSession session) {
+    @GetMapping("/user/mypage")
+    public String userSession(HttpSession session) {
         UserDTO userSession = (UserDTO) session.getAttribute("userSession");
         if (userSession != null) {
             return "user/user-mypage";
         } else {
-            return "index/index";
+            return "user/user-login";
         }
     }
 
-    @GetMapping("/userLogoutGo")
+    @GetMapping("/user/logout")
     public String userLogoutProcess(HttpSession session) {
         session.removeAttribute("userSession");
         return "redirect:/";
     }
 
-    @GetMapping("/userInfoFind")
+    @GetMapping("/user/info/find")
     public String userInfoFinding() {
         return "user/user-idfind";
     }
 
 
-    @PostMapping("/userJoinClick")
+    @PostMapping("/user/join")
     public String userLoginOK(UserDTO userDto) {
         return userService.registerUser(userDto);
     }
 
-    @PostMapping("/userLogSuccess")
+    @PostMapping("/user/login")
     @ResponseBody
     public ResponseEntity<Map<String, Object>> processLoginForm(@RequestParam("user_id") String userId,
                                                                 @RequestParam("user_pwd") String userPwd,
@@ -97,25 +96,25 @@ public class UserController {
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
-    @PostMapping("/userInfoUpdate")
+    @PostMapping("/user/update")
     public String userInfoUpdate(UserDTO userDto) {
         return userService.userInfoUpdate(userDto);
     }
 
 
-    @PostMapping("/userInfoDelete")
+    @PostMapping("/user/delete")
     public String userInfoDelete(UserDTO userDto, HttpSession session) {
         return userService.userInfoDelete(userDto, session);
     }
 
     @ResponseBody
-    @PostMapping("/userIdCheck")
+    @PostMapping("/user/id/check")
     public int idCheck(@RequestParam("user_id") String userId) {
         return userService.userIdCheck(userId);
     }
 
     @ResponseBody
-    @PostMapping("/userIdInfoFind")
+    @PostMapping("/user/id/find")
     public String userIdFindProcess(@RequestParam("user_name") String userName,
                                     @RequestParam("user_email") String userEmail,
                                     @RequestParam("user_jumin") String userJumin) {
--- src/main/resources/static/js/user/user-idfind.js
+++ src/main/resources/static/js/user/user-idfind.js
@@ -1,4 +1,3 @@
-// // 아이디와 비밀번호가 공백일 때 적용되는 자바스크립트 (광진)
 let name = $('#user_name');
 let email = $('#user_email');
 let jumin = $('#user_jumin');
