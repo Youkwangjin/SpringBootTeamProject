edb61f31359972c90c1a4cb8967208dfef7253c1
Kwangjin <kwangjin5468@naver.com>
2024-02-25 18:34:08 +0900

    Chore: Update contact URL to follow RESTful conventions

--- src/main/java/pack/controller/contact/ContactController.java
+++ src/main/java/pack/controller/contact/ContactController.java
@@ -2,34 +2,35 @@ package pack.controller.contact;
 
 import java.util.List;
 
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pack.dto.contact.ContactDTO;
 import pack.service.contact.ContactService;
 
 @Controller
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class ContactController {
 
     private final ContactService contactService;
 
-    @RequestMapping("/contactAdmin")
+    @RequestMapping("/contact/admin")
     public String adminList(Model model) {
         List<ContactDTO> list = contactService.listContact();
         model.addAttribute("list", list);
         return "contact/contact-admin";
     }
 
-    @RequestMapping("/contactUser")
+    @GetMapping("/contact/user")
     public String mainListUser(Model model) {
         List<ContactDTO> list = contactService.listContact();
         model.addAttribute("list", list);
         return "contact/contact-user";
     }
 
-    @RequestMapping("/contactOwner")
+    @GetMapping("/contact/owner")
     public String mainListOwner(Model model) {
         List<ContactDTO> list = contactService.listContact();
         model.addAttribute("list", list);
