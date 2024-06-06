4f6f87acbf58cea0a0537b602714c1588715ddc5
Kwangjin <kwangjin5468@naver.com>
2024-06-06 23:13:56 +0900

    Feat : 암호화, 경로 설정 추가

--- src/main/java/pack/config/SpringSecurityConfig.java
+++ src/main/java/pack/config/SpringSecurityConfig.java
@@ -4,16 +4,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig {
 
     @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
+                .csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
                 .authorizeHttpRequests((authorizeRequests) -> authorizeRequests
                         // Static Resource
                         .requestMatchers("/css/**",
@@ -22,9 +30,11 @@ public class SpringSecurityConfig {
                                          "/js/**",
                                          "/favicon.ico").permitAll()
                         .requestMatchers("/",
+                                         "/conajax",
                                          "/user/join",
+                                         "/user/register",
                                          "/user/login",
-                                         "owner/join",
+                                         "/owner/join",
                                          "/owner/login").permitAll()
                         .requestMatchers("/user/**").hasRole("USER")
                         .requestMatchers("/admin/**").hasRole("ADMIN")
