ed8baea5578ab969d4d2d535117338e3dee73515
Kwangjin <kwangjin5468@naver.com>
2024-06-16 14:41:11 +0900

    Chore : 상태 코드 message 값 변경

--- src/main/java/pack/common/enums/code/SuccessCode.java
+++ src/main/java/pack/common/enums/code/SuccessCode.java
@@ -1,4 +1,4 @@
-package pack.common.enums;
+package pack.common.enums.code;
 
 
 import lombok.Getter;
@@ -10,17 +10,18 @@ public enum SuccessCode {
      * ******************************* Success CodeList ***************************************
      */
 
-    // 조회 성공 코드 (HTTP Response: 200 OK)
-    SELECT_SUCCESS(200, "200", "SELECT SUCCESS"),
+    // 회원 가입 성공 코드 (HTTP Response: 201 Created)
+    REGISTER_INSERT_SUCCESS(201, "201", "회원가입이 정상적으로 완료되었습니다."),
+
+    // 수정 성공 코드 (HTTP Response: 204 OK)
+    USER_UPDATE_SUCCESS(204, "204", "회원정보가 성공적으로 수정되었습니다. 다시 로그인 해주세요."),
 
     // 삭제 성공 코드 (HTTP Response: 200 OK)
-    DELETE_SUCCESS(200, "200", "DELETE SUCCESS"),
+    USER_DELETE_SUCCESS(200, "200", "그동안 이용해 주셔서 감사합니다."),
 
-    // 삽입 성공 코드 (HTTP Response: 201 Created)
-    INSERT_SUCCESS(201, "201", "INSERT SUCCESS"),
+    // 조회 성공 코드 (HTTP Response: 200 OK)
+    SELECT_SUCCESS(200, "200", "SELECT SUCCESS");
 
-    // 수정 성공 코드 (HTTP Response: 201 Created)
-    UPDATE_SUCCESS(200, "204", "UPDATE SUCCESS");
 
     // 성공 코드의 '코드 상태'를 반환
     private final int status;
