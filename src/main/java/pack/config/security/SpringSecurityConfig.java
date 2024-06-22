74a35194e6215d9b7dbd8b3377ba306a6df3b322
Kwangjin <kwangjin5468@naver.com>
2024-06-22 22:45:56 +0900

    Chore : csrf 토큰 설정 클래스 경로 변경

--- src/main/java/pack/config/security/SpringSecurityConfig.java
+++ src/main/java/pack/config/security/SpringSecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import pack.security.csrf.LoggingCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
