1cd1f93e6d9f145f2fa9fabd7e55c8a99aa8208e
youkwangjin <kwangjin5468@gmail.com>
2024-08-16 23:52:59 +0900

    Chore : UserDTO -> User 변경

--- src/main/java/pack/service/user/impl/UserDetailsServiceImpl.java
+++ src/main/java/pack/service/user/impl/UserDetailsServiceImpl.java
@@ -5,9 +5,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import pack.dto.user.UserDTO;
+import pack.model.user.User;
 import pack.repository.user.UserRepository;
-import pack.security.CustomUserDetails;
 
 @Service
 @RequiredArgsConstructor
@@ -18,10 +17,10 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     // 로그인 처리
     @Override
     public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
-        UserDTO user = userRepository.findByUserEmail(userEmail);
+        User user = userRepository.findByUserEmail(userEmail);
         if (user == null) {
             throw new UsernameNotFoundException("User not found with email: " + userEmail);
         }
-        return new CustomUserDetails(user);
+        return user;  // User 객체를 바로 반환
     }
 }
