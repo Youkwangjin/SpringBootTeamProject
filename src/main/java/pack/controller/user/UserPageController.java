677271fccd69467844961ca5625942a9f4478bf9
youkwangjin <kwangjin5468@gmail.com>
2024-09-11 23:19:27 +0900

    Refactor :  HTML + CSS 수정 작업

--- src/main/java/pack/controller/user/UserPageController.java
+++ src/main/java/pack/controller/user/UserPageController.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import pack.model.user.User;
 import pack.service.user.UserService;
 
