7bb6f1d1a36fe5d97ee2c8f304ad64502d988b88
youkwangjin <kwangjin5468@gmail.com>
2024-09-15 20:29:10 +0900

    Chore : 회원 정보 수정 DTO 어노테이션 변경

--- src/main/java/com/acorn/api/dto/user/UserUpdateDTO.java
+++ src/main/java/com/acorn/api/dto/user/UserUpdateDTO.java
@@ -1,10 +1,10 @@
 package com.acorn.api.dto.user;
 
-import lombok.Builder;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@Builder
+@AllArgsConstructor
 public class UserUpdateDTO {
 
     private String userUUId;
