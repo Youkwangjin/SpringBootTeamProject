dcf4fb632dbdbec8907bf101580edac050e54570
youkwangjin <kwangjin5468@gmail.com>
2024-10-03 23:40:58 +0900

    Feat : 게시판 입력 데이터 검증 로직 추가 작업

---
+++ src/main/java/com/acorn/api/dto/board/BoardRegisterDTO.java
@@ -0,0 +1,25 @@
+package com.acorn.api.dto.board;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class BoardRegisterDTO {
+
+    @NotBlank
+    @Size(max = 50)
+    private String boardTitle;
+
+    @NotBlank
+    private String boardWriter;
+
+    @NotBlank
+    @Size(max = 6)
+    private String boardPassword;
+
+    @NotBlank
+    private String boardContents;
+}
