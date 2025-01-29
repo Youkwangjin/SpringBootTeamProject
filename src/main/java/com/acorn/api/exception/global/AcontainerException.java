c88be4affb7be6d38353aba0bae2a5e5e6490a87
youkwangjin <kwangjin5468@gmail.com>
2025-01-29 23:43:23 +0900

    Refactor : 커스텀 예외 처리 적용

--- src/main/java/com/acorn/api/exception/global/AcontainerException.java
+++ src/main/java/com/acorn/api/exception/global/AcontainerException.java
@@ -1,5 +1,7 @@
 package com.acorn.api.exception.global;
 
+import com.acorn.api.code.common.ApiErrorCode;
+import com.acorn.api.code.common.ApiHttpErrorCode;
 import com.acorn.api.code.common.ApiValidationErrorCode;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
@@ -14,6 +16,20 @@ public class AcontainerException extends RuntimeException {
     private final String errorMsg;
 
     public AcontainerException(ApiValidationErrorCode errorCode) {
+        super(errorCode.getErrorMsg());
+        this.httpStatus = errorCode.getHttpStatus();
+        this.errorDivisionCode = errorCode.getErrorDivisionCode();
+        this.errorMsg = errorCode.getErrorMsg();
+    }
+
+    public AcontainerException(ApiErrorCode errorCode) {
+        super(errorCode.getErrorMsg());
+        this.httpStatus = errorCode.getHttpStatus();
+        this.errorDivisionCode = errorCode.getErrorDivisionCode();
+        this.errorMsg = errorCode.getErrorMsg();
+    }
+
+    public AcontainerException(ApiHttpErrorCode errorCode) {
         super(errorCode.getErrorMsg());
         this.httpStatus = errorCode.getHttpStatus();
         this.errorDivisionCode = errorCode.getErrorDivisionCode();
