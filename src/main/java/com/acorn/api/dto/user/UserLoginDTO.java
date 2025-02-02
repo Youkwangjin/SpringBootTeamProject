745e28d93b81a4e143de7ccd6cf597b9e91c60c9
kwangjin <kwangjin5468@naver.com>
2025-02-02 21:47:43 +0900

    Feat : 기본 생성자 추가 작업

--- src/main/java/com/acorn/api/dto/user/UserLoginDTO.java
+++ src/main/java/com/acorn/api/dto/user/UserLoginDTO.java
@@ -2,9 +2,11 @@ package com.acorn.api.dto.user;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserLoginDTO {
 
     private String userEmail;
