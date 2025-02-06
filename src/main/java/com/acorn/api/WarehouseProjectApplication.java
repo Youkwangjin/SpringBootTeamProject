b6b8260b9b20d9dc43a2fc80d37007637b16136a
kwang <kwanjin5468@naver.com>
2025-02-06 14:10:49 +0900

    Refactor: MyBatis Mapper 방식으로 변경하여 유지보수성 개선

--- src/main/java/com/acorn/api/WarehouseProjectApplication.java
+++ src/main/java/com/acorn/api/WarehouseProjectApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WarehouseProjectApplication {
-	
-	// 리펙토링 시작
+
 	public static void main(String[] args) {
 		SpringApplication.run(WarehouseProjectApplication.class, args);
 	}
