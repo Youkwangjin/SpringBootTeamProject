0f4c7820046c709f8303825663b86caadb3a8c3c
kwangjin <kwangjin5468@naver.com>
2025-04-17 16:16:54 +0900

    Feat: 관리자 사용자 목록 페이지 구현

--- src/main/java/com/acorn/api/config/SpringSecurityConfig.java
+++ src/main/java/com/acorn/api/config/SpringSecurityConfig.java
@@ -148,7 +148,8 @@ public class SpringSecurityConfig {
                         // Protected Admin Common Pages
                         .requestMatchers("/admin/mypage",
                                          "/admin/container/list/**",
-                                         "/admin/container/detail/**").hasAuthority("ROLE_ADMIN")
+                                         "/admin/container/detail/**",
+                                         "/admin/user/list/**").hasAuthority("ROLE_ADMIN")
 
                         // Protected User API
                         .requestMatchers("/api/user/update/{userId}",
