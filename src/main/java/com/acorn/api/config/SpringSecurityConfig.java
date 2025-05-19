e345a2b55488293b77399936f591d914bb1ca123
kwangjin <kwangjin5468@naver.com>
2025-05-20 00:56:20 +0900

    Refactor: 네이버 지오코딩 API로 변경하여 주소 좌표 변환 처리

--- src/main/java/com/acorn/api/config/SpringSecurityConfig.java
+++ src/main/java/com/acorn/api/config/SpringSecurityConfig.java
@@ -100,6 +100,7 @@ public class SpringSecurityConfig {
                                          "/session/expired",
                                          "/service",
                                          "/containerMaps",
+                                         "/api/geocode/**",
                                          "/user/join",
                                          "/user/login",
                                          "/owner/join",
--- src/main/java/com/acorn/api/controller/common/CommonController.java
+++ src/main/java/com/acorn/api/controller/common/CommonController.java
@@ -2,10 +2,10 @@ package com.acorn.api.controller.common;
 
 import com.acorn.api.service.common.CommonService;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,12 +15,21 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class CommonController {
 
-    private static final Logger log = LoggerFactory.getLogger(CommonController.class);
     private final CommonService commonService;
+
+    @GetMapping("/api/geocode")
+    public ResponseEntity<?> getCoordinates(@RequestParam String address) {
+        log.info(" *****************************    NAVER Geocoding API START    *****************************");
+
+        String response = commonService.getCoordinatesFromNaverAPI(address);
+
+        return ResponseEntity.ok(response);
+    }
 
     @PostMapping("/api/editor/image/upload")
     public ResponseEntity<Map<String, Object>> uploadImage(@RequestParam("file") MultipartFile file) {
