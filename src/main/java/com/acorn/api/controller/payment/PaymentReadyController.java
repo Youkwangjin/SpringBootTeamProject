a73ecca6d505eb4a70f609bb72a12b461a2c1b21
kwangjin <kwangjin5468@naver.com>
2025-05-27 19:42:14 +0900

    Refactor: 창고 결제 준비 컨트롤러 클래스명 변경

--- src/main/java/com/acorn/api/controller/payment/PaymentReadyController.java
+++ src/main/java/com/acorn/api/controller/payment/PaymentReadyController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-public class PaymentController {
+public class PaymentReadyController {
 
     private final PaymentService paymentService;
 
