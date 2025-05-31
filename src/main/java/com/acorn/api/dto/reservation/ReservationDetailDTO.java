99d0be05b747ae9e2c35d59e02c906e83e5dc3eb
kwangjin <kwangjin5468@naver.com>
2025-05-31 17:47:23 +0900

    Refactor: 예약 상세 응답에 결제 정보 식별자 추가

--- src/main/java/com/acorn/api/dto/reservation/ReservationDetailDTO.java
+++ src/main/java/com/acorn/api/dto/reservation/ReservationDetailDTO.java
@@ -15,6 +15,8 @@ public class ReservationDetailDTO {
 
     private Integer reservationContainerId;
 
+    private Integer paymentId;
+
     private String containerName;
 
     private String containerAddr;
