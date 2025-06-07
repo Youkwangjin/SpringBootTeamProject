9c1bb2981ab0177e59d71e32cb8299c2ccc89307
kwangjin <kwangjin5468@naver.com>
2025-06-07 14:33:17 +0900

    Refactor: 공지사항 DTO 필드 유효성 어노테이션 보강

--- src/main/java/com/acorn/api/dto/board/BoardDeleteDTO.java
+++ src/main/java/com/acorn/api/dto/board/BoardDeleteDTO.java
@@ -1,5 +1,7 @@
 package com.acorn.api.dto.board;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,6 +13,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class BoardDeleteDTO {
 
+    @NotNull
+    @Positive
     private Integer boardId;
 
     private Integer boardUserId;
