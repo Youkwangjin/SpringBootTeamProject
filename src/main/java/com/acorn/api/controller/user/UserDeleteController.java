11047f8b01b0b7538855ee2fa19dee2cd35789cf
kwangjin <kwangjin5468@naver.com>
2025-06-13 11:20:48 +0900

    Refactor: 사용자 로직 컨트롤러 @Slf4j 적용 및 코드 정리

--- src/main/java/com/acorn/api/controller/user/UserDeleteController.java
+++ src/main/java/com/acorn/api/controller/user/UserDeleteController.java
@@ -3,27 +3,25 @@ package com.acorn.api.controller.user;
 import com.acorn.api.code.common.ApiSuccessCode;
 import com.acorn.api.code.response.ApiResponseBuilder;
 import com.acorn.api.code.response.ApiSuccessResponse;
-import com.acorn.api.dto.user.request.UserDeleteResDTO;
+import com.acorn.api.dto.user.request.UserDeleteReqDTO;
 import com.acorn.api.service.user.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class UserDeleteController {
 
-    private static final Logger log = LoggerFactory.getLogger(UserDeleteController.class);
     private final UserService userService;
 
     @PostMapping("/api/user/delete/{userId}")
-    public ResponseEntity<ApiSuccessResponse<Object>> userDelete(@Valid @RequestBody UserDeleteResDTO userDeleteData) {
-
+    public ResponseEntity<ApiSuccessResponse<Object>> userDelete(@Valid @RequestBody UserDeleteReqDTO userDeleteData) {
         log.info(" *****************************    User Delete START    *****************************");
 
         userService.userDataDelete(userDeleteData);
--- src/main/java/com/acorn/api/controller/user/UserUpdateController.java
+++ src/main/java/com/acorn/api/controller/user/UserUpdateController.java
@@ -3,27 +3,25 @@ package com.acorn.api.controller.user;
 import com.acorn.api.code.common.ApiSuccessCode;
 import com.acorn.api.code.response.ApiResponseBuilder;
 import com.acorn.api.code.response.ApiSuccessResponse;
-import com.acorn.api.dto.user.request.UserUpdateResDTO;
+import com.acorn.api.dto.user.request.UserUpdateReqDTO;
 import com.acorn.api.service.user.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class UserUpdateController {
 
-    private static final Logger log = LoggerFactory.getLogger(UserUpdateController.class);
     private final UserService userService;
 
     @PatchMapping("/api/user/update/{userId}")
-    public ResponseEntity<ApiSuccessResponse<Object>> userUpdate(@Valid @RequestBody UserUpdateResDTO userUpdateData) {
-
+    public ResponseEntity<ApiSuccessResponse<Object>> userUpdate(@Valid @RequestBody UserUpdateReqDTO userUpdateData) {
         log.info(" *****************************    User Update START    *****************************");
 
         userService.userDataUpdate(userUpdateData);
