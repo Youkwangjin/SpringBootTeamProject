f59a4ff4a02a7d5a583b385de90c466a59e65ca2
kwangjin <kwangjin5468@naver.com>
2025-06-13 11:26:03 +0900

    Refactor: Enum명 변경

--- src/main/java/com/acorn/api/controller/user/UserValidationController.java
+++ src/main/java/com/acorn/api/controller/user/UserValidationController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserValidationController {
 
-
     private final UserService userService;
 
     @GetMapping("/api/auth/user/emailCheck")
